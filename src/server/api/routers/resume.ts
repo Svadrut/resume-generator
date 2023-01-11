ab3cf34871253851ee2305b73c4707acf75361e2
Svadrut <svadrut@gmail.com>
2023-01-10 20:02:39 -0500

    Finished the app

---
+++ src/server/api/routers/resume.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { openai } from "../../../utils/openai";
+
+export const resumeRotuer = createTRPCRouter({
+  getResponse: publicProcedure
+    .input(z.object({ resumeText: z.string() }))
+    .mutation(async ({ input: { resumeText }, ctx: { prisma } }) => {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Generate a resume based on these experiences and acheivments: ${resumeText}`,
+        max_tokens: 256,
+        temperature: 0.7,
+      });
+      if (!response.data.choices) {
+        const error = new Error('Please try again later');
+        throw error;
+      } else {
+        await prisma.resume.create({
+          data: {
+            promtText: resumeText,
+            generatedText: response.data.choices[0]!.text!,
+          },
+        });
+
+        return response.data.choices[0]!.text;
+      }
+    }),
+});
